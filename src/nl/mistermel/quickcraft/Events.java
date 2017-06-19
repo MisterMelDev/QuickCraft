abfc3b3707792c332a04700fb846ef4847651ec2
Rens Mulder <mulderrens@outlook.com>
2017-06-19 17:26:36 +0200

    Events Pusherino

--- src/nl/mistermel/quickcraft/Events.java
+++ src/nl/mistermel/quickcraft/Events.java
@@ -1,7 +1,10 @@
 package nl.mistermel.quickcraft;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import nl.mistermel.quickcraft.utils.ArenaManager;
@@ -12,6 +15,24 @@ public class Events implements Listener {
 	public void onPlayerQuit(PlayerQuitEvent e) {
 		if(ArenaManager.isInGame(e.getPlayer())) {
 			ArenaManager.getArena(e.getPlayer()).leave(e.getPlayer());
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerDrop(PlayerDropItemEvent e) {
+		if(ArenaManager.isInGame(e.getPlayer())) {
+			e.setCancelled(true);
+			e.getPlayer().sendMessage(QuickCraft.PREFIX + ChatColor.RED +
+					"You can't drop items ingame!");
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerPickup(PlayerPickupItemEvent e) {
+		if(ArenaManager.isInGame(e.getPlayer())) {
+			e.setCancelled(true);
+			e.getPlayer().sendMessage(QuickCraft.PREFIX + ChatColor.RED +
+					"You can't pickup items ingame!");
 		}
 	}
 	
